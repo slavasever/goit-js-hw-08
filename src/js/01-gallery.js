a0521eaf2b88bd5be94dc80b7ab0da2b1383856b
Viacheslav Severyn <sever_2008@ukr.net>
2022-02-19 20:19:55 +0200

    hw-08_done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,24 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+function render(arr) {
+  const markup = arr
+    .map(
+      ({ original, preview, description }) =>
+        `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" title="${description}" /></a>`,
+    )
+    .join('');
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+render(galleryItems);
+
+let gallery = new SimpleLightbox('.gallery a', { captionDelay: 250 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,60 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+const storage = loadFromLocalStorage('feedback-form-state');
+
+// console.log(storage);
+
+function formFill(obj) {
+  if (obj) {
+    refs.form.elements.email.value = obj.email;
+    refs.form.elements.message.value = obj.message;
+  } else {
+    refs.form.elements.email.value = '';
+    refs.form.elements.message.value = '';
+  }
+}
+
+formFill(storage);
+
+refs.form.addEventListener('input', throttle(handleInput, 500));
+refs.form.addEventListener('submit', handleSubmit);
+
+function handleInput() {
+  const obj = {
+    email: refs.form.elements.email.value,
+    message: refs.form.elements.message.value,
+  };
+  saveToLocalStorage('feedback-form-state', obj);
+}
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(loadFromLocalStorage('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state');
+  refs.form.reset();
+}
+
+function saveToLocalStorage(key, value) {
+  try {
+    const localStorageValue = JSON.stringify(value);
+    localStorage.setItem(key, localStorageValue);
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
+
+function loadFromLocalStorage(key) {
+  try {
+    const localStorageValue = localStorage.getItem(key);
+    return localStorageValue === null ? undefined : JSON.parse(localStorageValue);
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
